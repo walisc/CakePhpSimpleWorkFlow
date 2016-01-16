85c49dd5a2639f306b23fb4f30edcc192aa62197
chido <chidow@j5int.com>
2016-01-16 14:07:05 +0200

    getting workflow actions

--- Controller/Component/DALComponent.php
+++ Controller/Component/DALComponent.php
@@ -16,12 +16,35 @@ class DALComponent extends Component
 
         $this->WorkflowActions = ClassRegistry::init('SimpleWorkflow.WorkflowActions');
         $this->UserWorkflowActions = ClassRegistry::init('SimpleWorkflow.UserWorkflowActions');
+        $this->MapUserActionWorkflow = ClassRegistry::init('SimpleWorkflow.MapUserActionWorkflow');
 
     }
 
-    public function GetAllWorkflowActions()
+    public function GetAllWorkflowActions($workflow_id, $user_id)
     {
-        return $this->UserWorkflowActions->find("all");
+        $options = array("conditions" => array("MapUserActionWorkflow.is_active" => 1,
+                                              "MapUserActionWorkflow.workflow_ref" => $workflow_id,
+                                              "MapUserActionWorkflow.user_id" => $user_id));
+
+        $user_workflow = $this->MapUserActionWorkflow->find("first", $options);
+
+        if ($user_workflow["MapUserActionWorkflow"]["workflow_completed"])
+        {
+            return array("Completed");
+        }
+
+        $possible_actions_ref = array();
+
+        foreach($user_workflow["WORKFLOW_MAP_USER_WA"] as $ac_ref)
+        {
+            if ($ac_ref["action_complete"] == 0 && $ac_ref["action_processed"] == 0)
+            {
+                array_push($possible_actions_ref, $ac_ref["workflow_action_ref"]);
+            }
+        }
+
+        //TODO: cache all the workflow actions
+        return $this->WorkflowActions->find("all", array("conditions" => array("WorkflowActions.id" => $possible_actions_ref)));
     }
 }
 
